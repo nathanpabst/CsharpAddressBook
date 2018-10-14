e7db8a00604bdb92efe6b2b36c2b2943333a568f
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-14 15:06:47 -0500

    add and exit commands working properly

---
+++ CsharpAddressBook/Peeps/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpAddressBook.Peeps
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        public Person(string firstName, string lastName, string phone, string address, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            Address = address;
+            Email = email;
+        }
+
+    }
+}
